27296921af5c8e65b631b9e54819eeae969d251e
jamesjara <JAMESJARA@gmail.com>
2020-04-02 15:45:33 -0600

    v3

--- android/src/main/java/com/reactnativesupportlibcore/RNCoreSupportModule.java
+++ android/src/main/java/com/reactnativesupportlibcore/RNCoreSupportModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactnativesupportlibcore;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/reactnativesupportlibcore/RNCoreSupportPackage.java
+++ android/src/main/java/com/reactnativesupportlibcore/RNCoreSupportPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactnativesupportlibcore;
 
 import java.util.Arrays;
 import java.util.Collections;
